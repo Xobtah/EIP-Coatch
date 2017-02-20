60c748fc2b230734db5551eb8b8d51e87477ed27
sylvain garant <sylvain.garant@epitech.eu>
2017-02-20 13:05:03 +0100

    Basic application

--- server/app.js
+++ server/app.js
@@ -2,11 +2,18 @@
  * Created by xobtah on 31/01/17.
  */
 
-let server = require('http');
 let express = require('express');
 let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+
+server.listen(8080);
 
 app.use(express.static('client'));
 app.use('/node_modules', express.static('node_modules'));
 
-app.listen(8080);
+io.on('connection', socket => {
+    console.log('Connexion');
+    socket.emit('news', { hello: 'world' });
+    socket.on('data', data => console.log(data) );
+});
